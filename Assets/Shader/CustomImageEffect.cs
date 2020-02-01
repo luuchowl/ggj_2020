b48c7be67b7cccabd5e07b6f3ab9097d830d6220
Deivede Tibola <deivedetibola@gmail.com>
2020-02-01 10:55:56 -0300

    Cenas

---
+++ Assets/Shader/CustomImageEffect.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CustomImageEffect : MonoBehaviour
+{
+
+    public Material material;
+    public Camera cam;
+
+    private void Awake()
+    {
+        cam.depthTextureMode = DepthTextureMode.DepthNormals;
+    }
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+
+        Graphics.Blit(src, dest, material);
+    }
+}
