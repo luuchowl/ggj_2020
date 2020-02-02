89ad20fbda725205d24de2be1919234ebdcf66f6
lucaslam <lucas_lam@live.com>
2020-02-02 14:39:36 -0300

    FMOD

---
+++ Assets/_Scripts/UpgradeMenu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeMenu : MonoBehaviour
+{
+    public void UpgradeShoot;
+    public void UpgradeDash;
+    public void UpgradeSword;
+
+}
