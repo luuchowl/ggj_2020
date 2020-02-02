854ca0821caee062d06a2b41467c7125f4f4ff80
Deive_Ex <feliperobos@gmail.com>
2020-02-02 02:26:00 -0300

    Text Mesh Pro Essentials

---
+++ Assets/_Scripts/Game_Manager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : Singleton<Game_Manager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/GoToScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToScene : MonoBehaviour
+{
+
+    public void ChangeScene(string scenenName)
+	{
+		SceneManager.LoadScene(scenenName);
+	}
+}
