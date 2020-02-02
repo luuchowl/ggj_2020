a2d6445161fa0b595c92f1f3edecf9022bcb6fef
Deive_Ex <feliperobos@gmail.com>
2020-02-02 15:31:04 -0300

    meh

--- Assets/_Scripts/UpgradeMenu.cs
+++ Assets/_Scripts/UpgradeMenu.cs
@@ -8,24 +8,55 @@ public class UpgradeMenu : MonoBehaviour
     public PlayerStatus status;
     public Button buttonDash;
     public Button buttonSword;
-    public Button sword;
+    public Button buttonShoot;
 
-    public void Start()
+    public Animator parentAnimator;
+    public GameObject fader;
+
+    public void Awake()
     {
         status = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStatus>();
+    }
+
+    public void OnEnable()
+    {
+        buttonDash.interactable = !status.upgradeDodge;
+        buttonSword.interactable = !status.upgradeSword;
+        buttonShoot.interactable = !status.upgradeShoot;
+        fader.SetActive(false);
     }
 
     public void UpgradeShoot()
     {
         status.upgradeShoot = true;
+        buttonDash.interactable = false;
+        buttonSword.interactable = false;
+        buttonShoot.interactable = false;
+        StartCoroutine(FadeOutRoutine());
     }
     public void UpgradeDash()
     {
         status.upgradeDodge = true;
+        buttonDash.interactable = false;
+        buttonSword.interactable = false;
+        buttonShoot.interactable = false;
+        StartCoroutine(FadeOutRoutine());
     }
     public void UpgradeSword()
     {
         status.upgradeSword = true;
+        buttonDash.interactable =  false;
+        buttonSword.interactable = false;
+        buttonShoot.interactable = false;
+        StartCoroutine(FadeOutRoutine());
+    }
+
+    IEnumerator FadeOutRoutine()
+    {
+        yield return null;
+        parentAnimator.Play("FadeOut");
+        yield return new WaitForSeconds(1f);
+        parentAnimator.gameObject.SetActive(false);
     }
 
 }
