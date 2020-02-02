e16fdb33e77941c88d0bd1acde17ec43ea3f9383
Deive_Ex <feliperobos@gmail.com>
2020-02-02 13:04:49 -0300

    Help Object

--- Assets/_Scripts/Game_Manager.cs
+++ Assets/_Scripts/Game_Manager.cs
@@ -6,6 +6,7 @@ using UnityEngine.Events;
 
 public class Game_Manager : Singleton<Game_Manager>
 {
+	[HideInInspector] public Transform player;
 	[HideInInspector] public PlayerInputController playerInput;
 	[HideInInspector] public PlayerStatus playerStatus;
 
@@ -19,6 +20,7 @@ public class Game_Manager : Singleton<Game_Manager>
 
 	private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
 	{
+		player = GameObject.FindGameObjectWithTag("Player").transform;
 		playerInput = FindObjectOfType<PlayerInputController>();
 		playerStatus = FindObjectOfType<PlayerStatus>();
 
